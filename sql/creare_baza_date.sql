dfd0b8a16fd58e6e993d28e5c35ca8ac3411960f
Rares Mascovici <raresmascovici@gmail.com>
2026-03-13 16:00:19 +0200

    Am actualizat baza de date

--- sql/creare_baza_date.sql
+++ sql/creare_baza_date.sql
@@ -19,9 +19,22 @@ salariu_max decimal(10,2),
 foreign key (id_companie) references Companii(id_companie) on delete cascade
 );
 
+create table Candidati(
+id_candidat int primary key auto_increment,
+nume varchar(50) not null,
+prenume varchar(50) not null,
+email varchar(50) unique,
+telefon varchar(20),
+cv_text longtext,
+fulltext(cv_text) 
+);
+
 create table Aplicatii(
 id_aplicatie int primary key auto_increment,
 id_job int,
 id_candidat int,
 data_aplicarii datetime default current_timestamp,
-status enum('Nou','Interviu,
+status enum('Nou','Interviu','Oferta','Respins') default 'Nou',
+foreign key (id_job) references Joburi(id_job),
+foreign key (id_candidat) references Candidati(id_candidat)
+);
