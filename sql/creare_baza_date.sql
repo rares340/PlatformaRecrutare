e400729d4dd5b5854793972f44acaa96106d93a2
Rares Mascovici <raresmascovici@gmail.com>
2026-03-13 15:54:18 +0200

    Am creat baza de date si structura de foldere

---
+++ sql/creare_baza_date.sql
@@ -0,0 +1,27 @@
+create database if not exists platforma_recrutare;
+use platforma_recrutare;
+
+create table Companii(
+id_companie int primary key auto_increment,
+nume varchar(100) not null,
+industrie varchar (50),
+adresa text
+);
+
+create table Joburi(
+id_job int primary key auto_increment,
+id_companie int,
+titlu varchar(100) not null,
+descriere text,
+oras varchar(50),
+salariu_min decimal(10,2),
+salariu_max decimal(10,2),
+foreign key (id_companie) references Companii(id_companie) on delete cascade
+);
+
+create table Aplicatii(
+id_aplicatie int primary key auto_increment,
+id_job int,
+id_candidat int,
+data_aplicarii datetime default current_timestamp,
+status enum('Nou','Interviu,
